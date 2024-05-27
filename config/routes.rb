cc0ed83a9a34ffaddc9eb750c51fba8fadc9a14c
Adam Wheatley <adamwheatley7@gmail.com>
2024-05-27 10:59:40 +0100

    Simplified header Changes to home page Updated routes to handle sign out

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   
   resources :locations
 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Defines the root path route ("/")
   root "home#index"
 end
